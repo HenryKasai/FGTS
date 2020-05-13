505530067f1cfbf08042039e9b5fd7c6a0f1c6e9
HenryKasai <henrykasai1775@gmail.com>
2020-05-13 09:33:51 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Insira seu salário e veja sua parcela do FGTS.");
+
+            string salário = Console.ReadLine();
+            decimal salario = Convert.ToDecimal(salário);
+            string oito = "8";
+            decimal oitoo = Convert.ToDecimal(oito);
+            string cem = "100";
+            decimal ceem = Convert.ToDecimal(cem);
+            decimal resultado = salario * oitoo / ceem;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Seu FGTS é de R$ " + resultado + ",00");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
